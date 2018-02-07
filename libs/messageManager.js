94d83e141935633c5e5c63f812e926cb25ac0c93
bitsoko <bitsoko@bitsoko.io>
2018-02-07 13:37:59 +0300

    Update messageManager.js

--- libs/messageManager.js
+++ libs/messageManager.js
@@ -1,1 +1,11 @@
+var exports = module.exports = {};
 
+exports.init = function() {
+   
+    
+    setTimeout(function(){
+    console.log('INFO: starting message manager');
+     //exports.orderBot();
+     
+    }, 10000);
+}
