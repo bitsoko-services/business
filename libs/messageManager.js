259a20bd951f48d942ac36ade01b7eb87e4e7003
bitsoko <bitsoko@bitsoko.io>
2018-03-16 15:54:05 +0300

    Update messageManager.js

--- libs/messageManager.js
+++ libs/messageManager.js
@@ -2,7 +2,101 @@ var exports = module.exports = {};
 
 
 
+exports.contByAdr = function(c,d) {
+    
+  var deferred = new Deferred();
+    try{
+       
+     
+bsConn.mysql.query('SELECT * FROM blockchains WHERE status = ? AND contractAddress = ?',['active',c],
+        function(err, results) { 
+          if (err)deferred.reject(err);
+	
+            deferred.resolve({res:results[0],ret:d});
+        }); 
+    }catch(e){
+       deferred.reject(e); 
+    }
+    
+    
+    return deferred;
+}
 
+
+exports.merchantOwner = function(d,e) {
+    
+  var deferred = new Deferred();
+    try{
+	    
+       d=parseInt(d);
+	    if(isNaN(d)){
+	 
+       deferred.reject({ret:e}); 
+		return;   
+	    }
+     
+bsConn.mysql.query('SELECT * FROM users WHERE id = ?', [d],
+        function(err, results) {  
+          if (err){
+		  deferred.reject({ret:e});
+		   return;
+	  }else if(results.length==0){
+	  
+		  deferred.reject({ret:e});
+		   return;
+	  }
+	//console.log(results);
+	var c={};
+	var removeNewline = require('newline-remove');
+	//removeNewline(results[0].googleMeta)
+	//var str = JSON.stringify(results[0].googleMeta).replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
+	try{
+     
+	//console.log(str);
+	var str = removeNewline(results[0].googleMeta);
+	try{
+		c.cover = JSON.parse(str).cover.url;
+		//c.tagline = JSON.parse(str).tagline;
+	}catch(err){
+	
+		c.cover = 'not available';
+		c.tagline = results[0].tagLine;
+	}
+	c.icon = JSON.parse(str).image;
+	c.email = JSON.parse(str).email;
+	c.address = results[0].wallets;
+	c.contractRate = results[0].contractRate;
+	c.phone = results[0].phoneNumber;
+	c.name = JSON.parse(str).name;
+	c.showTokens = results[0].showTokens;
+	c.showManagers = results[0].showManagers;
+	c.entIconList = results[0].entIconList;
+	c.entImageList = results[0].entImageList;
+	c.entAboutBody = results[0].entAboutBody;
+	c.entAboutTitle = results[0].entAboutTitle;
+		
+		try{
+	c.domains = JSON.parse(results[0].domains);
+	}catch(err){
+	
+	c.domains = JSON.parse('[]');
+	}
+	
+	//console.log(str);
+	}catch(e){
+       deferred.reject({ret:e}); 
+		return;
+    }
+	
+            deferred.resolve({res:c,ret:e});
+        }); 
+    }catch(e){
+       deferred.reject({ret:e}); 
+    }
+    
+    
+    return deferred;
+}
 
 exports.doPush = function(message,pid) {
 
