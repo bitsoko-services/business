6ea65c91c7beff1c635d93984ee8d698f8da9930
bitsoko <bitsoko@bitsoko.io>
2018-03-25 23:50:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ gcm = require('node-gcm');
 googlePushKey = 'AAAAbt9hX9o:APA91bE-V876epaCGolDrXSsbb0gXlnLM46BqAU-3H9MudpCru6pbEXaNHW5OBiNgDvDbNShnQo3Q3PMRicmp2itH7tW0IYU83i3WNgPdW_5zZHjVrJlGy9RwhUA7aX-PAMYWhrqh7qP5yF9LRseM34ILObz9V4vYA';
 fileDownloader = require('download-file');
 html2jade = require('html2jade');
+
+//Global variables
+var connectionSQL;
 stores = [];
 Cid = '476194103258-98t0j7p1lrela49ispgj3jfokl2r3ils.apps.googleusercontent.com';
 writeFile = require('write');
