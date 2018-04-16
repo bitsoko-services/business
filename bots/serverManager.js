058e02fe6275e0a7612bfddd55a38aa1e096d410
bitsoko <bitsoko@bitsoko.io>
2018-04-16 14:49:55 +0300

    Update serverManager.js

--- bots/serverManager.js
+++ bots/serverManager.js
@@ -3,11 +3,30 @@ var exports = module.exports = {};
 
 
 exports.logCleanerBot = function() {
+    
     // This bot saves the server logs to db then deletes the server logs folder every one hour
+    // TO-DO send logs to server
+    
+    
    var dura=heartBeat*(3*60) 
  setInterval(function(){ 
  
  console.log('deleting logs');
+     
+     var clearC = `
+            cd logs
+            rm -rf business.log
+        `;
+     
+    nCmd.get(clearC, function (data, err, stderr) {
+        if (!err) {
+            var hMsg = 'deleted logs';
+            console.log(hMsg);
+            
+        } else {
+            console.log(err);
+        }
+    });
      
      
  }, dura);   
