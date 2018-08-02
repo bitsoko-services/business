4cffbef490cc717fa23d5465b228888a0d7e7654
bitsoko <bitsoko@bitsoko.io>
2018-08-02 17:48:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -594,7 +594,7 @@ ReqRes = function ReqRes(req, res) {
 try {
 		    
          //getting store index page information
-var sendFl= __dirname + '/tmp/html/bits/?s='+getBitsWinOpt(req.url,'s')+'.html';
+var sendFl= __dirname + '/bitsAssets/tmp/html/bits/?s='+getBitsWinOpt(req.url,'s')+'.html';
 	console.log(sendFl);
 fs.accessSync(sendFl, fs.F_OK);	     
    res.sendFile(sendFl);
