59cec8710aeb764797f5b141c38546e8f15e8c12
gaseema-n <gaseema@bitsoko.io>
2018-08-08 15:16:11 +0300

    ent icon list update

--- app.js
+++ app.js
@@ -604,6 +604,10 @@ ReqRes = function ReqRes(req, res) {
             fs.readFile(__dirname + '/themes/default/templates/index.amp.pug', function (error, source) {
                 //TO-DO switch to new default
                 //fs.readFile(__dirname + '/themes/default/templates/index.amp.pug', function (error, source) {
+                console.log("/////////////////////////////////////////")
+                console.log(entSettings.entImageList)
+                console.log(entSettings.entImageList[0])
+                console.log(entSettings.entImageList[0].title)
                 matchShops();
                 var data = {
                     name: allInfo.name,
