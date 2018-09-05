f037a6ec2f98170d28be2a4896cc1953e7450f43
gaseema.n <gaseema@bitsoko.io>
2018-09-05 12:55:28 +0300

    changed main domain

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ primaryColor = '#0f5f76';
 
 heartBeat = 20000;
 var bitsokoEmail = 'bitsokokenya@gmail.com';
-var mainDomain = 'https://35.194.249.229:443'
+var mainDomain = 'https://bitsoko.co.ke'
 var compress = require('compression');
 nCmd = require('node-cmd');
 https = require('https');
